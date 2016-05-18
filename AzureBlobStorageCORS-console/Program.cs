7dacc41b68cd14262971b9c757587fe53f6b6860
Cedric GHIOT <Cedric.GHIOT@aldautomotive.com>
2016-05-18 15:54:51 +0200

    Initial Release

--- AzureBlobStorageCORS-console/Program.cs
+++ AzureBlobStorageCORS-console/Program.cs
@@ -1,15 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Shared.Protocol;
+using System;
 
 namespace AzureBlobStorageCORS_console
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string account = @"accountname";
+            string key = @"putdakeyhere";
+            var sc = new StorageCredentials(account, key);
+            var csa = new CloudStorageAccount(sc, true);
+            ConfigureCors(csa);
+            Console.ReadLine();
+        }
+
+        private static void ConfigureCors(CloudStorageAccount storageAccount)
+        {
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var serviceProperties = blobClient.GetServiceProperties();
+
+            foreach (var rule in serviceProperties.Cors.CorsRules)
+            {
+                foreach (var origin in rule.AllowedOrigins)
+                {
+                    Console.WriteLine(string.Format("{0}", origin));
+                }
+            }
+
+            var cors = new CorsRule();
+
+            cors.AllowedOrigins.Add("myald.be");
+            //cors.AllowedOrigins.Add("*");
+            cors.AllowedMethods = CorsHttpMethods.Get;
+            cors.MaxAgeInSeconds = 3600;
+
+            serviceProperties.Cors.CorsRules.Add(cors);
+
+            blobClient.SetServiceProperties(serviceProperties);
+
+            foreach (var rule in serviceProperties.Cors.CorsRules)
+            {
+                foreach (var origin in rule.AllowedOrigins)
+                {
+                    Console.WriteLine(string.Format("{0}", origin));
+                }
+            }
         }
     }
 }
