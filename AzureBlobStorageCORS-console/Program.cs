60d169b626040b707a7fafe20539c41f0a119ec7
Cedric GHIOT <Cedric.GHIOT@aldautomotive.com>
2016-05-20 08:11:58 +0200

    Add Clear and more verbose

--- AzureBlobStorageCORS-console/Program.cs
+++ AzureBlobStorageCORS-console/Program.cs
@@ -9,14 +9,14 @@ namespace AzureBlobStorageCORS_console
     {
         private static void Main(string[] args)
         {
-            string account = @"123";
-            string key = @"456";
+            string account = @"789";
+            string key = @"123";
             var sc = new StorageCredentials(account, key);
             var csa = new CloudStorageAccount(sc, true);
 
-            ReadCors(csa);
-            
+            ReadCors(csa);            
             //ConfigureCors(csa);
+
             Console.ReadLine();
         }
         private static void ReadCors(CloudStorageAccount storageAccount)
@@ -24,13 +24,17 @@ namespace AzureBlobStorageCORS_console
             var blobClient = storageAccount.CreateCloudBlobClient();
             var serviceProperties = blobClient.GetServiceProperties();
 
+            Console.WriteLine("----List-Rule-Begin---");
             foreach (var rule in serviceProperties.Cors.CorsRules)
             {
+                Console.WriteLine("----Rule-Begin---");
                 foreach (var origin in rule.AllowedOrigins)
                 {
                     Console.WriteLine(string.Format("{0}", origin));
                 }
+                Console.WriteLine("----Rule-End---");
             }
+            Console.WriteLine("----List-Rule-End---");
         }
 
 
@@ -39,35 +43,45 @@ namespace AzureBlobStorageCORS_console
             var blobClient = storageAccount.CreateCloudBlobClient();
             var serviceProperties = blobClient.GetServiceProperties();
 
+            Console.WriteLine("----List-Rule-Begin---");
             foreach (var rule in serviceProperties.Cors.CorsRules)
             {
+                Console.WriteLine("----Rule-Begin---");
                 foreach (var origin in rule.AllowedOrigins)
                 {
                     Console.WriteLine(string.Format("{0}", origin));
                 }
+                Console.WriteLine("----Rule-End---");
             }
+            Console.WriteLine("----List-Rule-End---");
+
+            //Use to clear all rules first
+            //serviceProperties.Cors.CorsRules.Clear();
 
             Console.WriteLine("---");
 
             var cors = new CorsRule();
-
             cors.AllowedOrigins.Add("null");
-            //cors.AllowedOrigins.Add("myald.be");
-            //cors.AllowedOrigins.Add("*");
+            cors.AllowedOrigins.Add("www.example.org");
+
             cors.AllowedMethods = CorsHttpMethods.Get;
             cors.MaxAgeInSeconds = 3600;
 
             serviceProperties.Cors.CorsRules.Add(cors);
-
             blobClient.SetServiceProperties(serviceProperties);
 
+
+            Console.WriteLine("----List-Rule-Begin---");
             foreach (var rule in serviceProperties.Cors.CorsRules)
             {
+                Console.WriteLine("----Rule-Begin---");
                 foreach (var origin in rule.AllowedOrigins)
                 {
                     Console.WriteLine(string.Format("{0}", origin));
                 }
+                Console.WriteLine("----Rule-End---");
             }
+            Console.WriteLine("----List-Rule-End---");
 
             Console.WriteLine("---");
         }
